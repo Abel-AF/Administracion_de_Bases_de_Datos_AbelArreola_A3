6e70471147d1edcd422f37927e45b851e9613159
Abel Arreola Flores <arreolaabel01@gmail.com>
2026-06-01 18:07:02 -0600

    Rename script.sql to AkirasBoutique.sql

---
+++ AkirasBoutique.sql
@@ -0,0 +1,235 @@
+﻿USE [master]
+GO
+/****** Object:  Database [AkirasBoutiques]    Script Date: 01/06/2026 04:00:14 p. m. ******/
+CREATE DATABASE [AkirasBoutiques]
+ CONTAINMENT = NONE
+ ON  PRIMARY 
+( NAME = N'AkirasBoutiques', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL17.MSSQLSERVER\MSSQL\DATA\AkirasBoutiques.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
+ LOG ON 
+( NAME = N'AkirasBoutiques_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL17.MSSQLSERVER\MSSQL\DATA\AkirasBoutiques_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
+ WITH CATALOG_COLLATION = DATABASE_DEFAULT, LEDGER = OFF
+GO
+ALTER DATABASE [AkirasBoutiques] SET COMPATIBILITY_LEVEL = 150
+GO
+IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
+begin
+EXEC [AkirasBoutiques].[dbo].[sp_fulltext_database] @action = 'enable'
+end
+GO
+ALTER DATABASE [AkirasBoutiques] SET ANSI_NULL_DEFAULT OFF 
+GO
+ALTER DATABASE [AkirasBoutiques] SET ANSI_NULLS OFF 
+GO
+ALTER DATABASE [AkirasBoutiques] SET ANSI_PADDING OFF 
+GO
+ALTER DATABASE [AkirasBoutiques] SET ANSI_WARNINGS OFF 
+GO
+ALTER DATABASE [AkirasBoutiques] SET ARITHABORT OFF 
+GO
+ALTER DATABASE [AkirasBoutiques] SET AUTO_CLOSE OFF 
+GO
+ALTER DATABASE [AkirasBoutiques] SET AUTO_SHRINK OFF 
+GO
+ALTER DATABASE [AkirasBoutiques] SET AUTO_UPDATE_STATISTICS ON 
+GO
+ALTER DATABASE [AkirasBoutiques] SET CURSOR_CLOSE_ON_COMMIT OFF 
+GO
+ALTER DATABASE [AkirasBoutiques] SET CURSOR_DEFAULT  GLOBAL 
+GO
+ALTER DATABASE [AkirasBoutiques] SET CONCAT_NULL_YIELDS_NULL OFF 
+GO
+ALTER DATABASE [AkirasBoutiques] SET NUMERIC_ROUNDABORT OFF 
+GO
+ALTER DATABASE [AkirasBoutiques] SET QUOTED_IDENTIFIER OFF 
+GO
+ALTER DATABASE [AkirasBoutiques] SET RECURSIVE_TRIGGERS OFF 
+GO
+ALTER DATABASE [AkirasBoutiques] SET  DISABLE_BROKER 
+GO
+ALTER DATABASE [AkirasBoutiques] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
+GO
+ALTER DATABASE [AkirasBoutiques] SET DATE_CORRELATION_OPTIMIZATION OFF 
+GO
+ALTER DATABASE [AkirasBoutiques] SET TRUSTWORTHY OFF 
+GO
+ALTER DATABASE [AkirasBoutiques] SET ALLOW_SNAPSHOT_ISOLATION OFF 
+GO
+ALTER DATABASE [AkirasBoutiques] SET PARAMETERIZATION SIMPLE 
+GO
+ALTER DATABASE [AkirasBoutiques] SET READ_COMMITTED_SNAPSHOT OFF 
+GO
+ALTER DATABASE [AkirasBoutiques] SET HONOR_BROKER_PRIORITY OFF 
+GO
+ALTER DATABASE [AkirasBoutiques] SET RECOVERY SIMPLE 
+GO
+ALTER DATABASE [AkirasBoutiques] SET  MULTI_USER 
+GO
+ALTER DATABASE [AkirasBoutiques] SET PAGE_VERIFY CHECKSUM  
+GO
+ALTER DATABASE [AkirasBoutiques] SET DB_CHAINING OFF 
+GO
+ALTER DATABASE [AkirasBoutiques] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
+GO
+ALTER DATABASE [AkirasBoutiques] SET TARGET_RECOVERY_TIME = 60 SECONDS 
+GO
+ALTER DATABASE [AkirasBoutiques] SET DELAYED_DURABILITY = DISABLED 
+GO
+ALTER DATABASE [AkirasBoutiques] SET OPTIMIZED_LOCKING = OFF 
+GO
+ALTER DATABASE [AkirasBoutiques] SET ACCELERATED_DATABASE_RECOVERY = OFF  
+GO
+ALTER DATABASE [AkirasBoutiques] SET QUERY_STORE = OFF
+GO
+USE [AkirasBoutiques]
+GO
+/****** Object:  Table [dbo].[categoria]    Script Date: 01/06/2026 04:00:15 p. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[categoria](
+	[id_categoria] [int] NOT NULL,
+	[nombre] [text] NOT NULL,
+	[descripcion] [text] NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[id_categoria] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[cliente]    Script Date: 01/06/2026 04:00:15 p. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[cliente](
+	[id_cliente] [int] NOT NULL,
+	[nombre] [text] NOT NULL,
+	[apellido] [text] NOT NULL,
+	[direccion] [text] NOT NULL,
+	[fec_nac] [date] NOT NULL,
+	[telefono] [int] NOT NULL,
+	[email] [text] NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[id_cliente] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[detalle]    Script Date: 01/06/2026 04:00:15 p. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[detalle](
+	[id_detalle] [int] NOT NULL,
+	[id_producto] [int] NOT NULL,
+	[cantidad] [int] NOT NULL,
+	[precio] [int] NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[id_detalle] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[empleados]    Script Date: 01/06/2026 04:00:15 p. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[empleados](
+	[id_empleado] [int] IDENTITY(1,1) NOT NULL,
+	[nombre] [varchar](100) NOT NULL,
+	[direccion] [varchar](150) NOT NULL,
+	[telefono] [varchar](15) NOT NULL,
+	[edad] [int] NOT NULL,
+	[correo_electronico] [varchar](100) NOT NULL,
+	[contrasena] [varchar](50) NOT NULL,
+	[id_sucursal] [int] NOT NULL,
+ CONSTRAINT [PK_empleado] PRIMARY KEY CLUSTERED 
+(
+	[id_empleado] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[factura]    Script Date: 01/06/2026 04:00:15 p. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[factura](
+	[id_factura] [int] NOT NULL,
+	[id_cliente] [int] NOT NULL,
+	[fecha] [date] NULL,
+	[id_detalle] [int] NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[id_factura] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[producto]    Script Date: 01/06/2026 04:00:15 p. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[producto](
+	[id_producto] [int] NOT NULL,
+	[nombre] [text] NOT NULL,
+	[precio] [int] NOT NULL,
+	[stock] [int] NOT NULL,
+	[id_categoria] [int] NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[id_producto] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[sucursal]    Script Date: 01/06/2026 04:00:15 p. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[sucursal](
+	[id_sucursal] [int] NOT NULL,
+	[nombre_sucursal] [varchar](100) NOT NULL,
+	[nombre_encargado] [varchar](100) NOT NULL,
+	[direccion] [varchar](150) NOT NULL,
+	[telefono] [varchar](15) NOT NULL,
+	[ciudad] [varchar](60) NOT NULL,
+	[estado] [varchar](60) NOT NULL,
+ CONSTRAINT [PK_sucursal] PRIMARY KEY CLUSTERED 
+(
+	[id_sucursal] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[detalle]  WITH CHECK ADD  CONSTRAINT [id_producto] FOREIGN KEY([id_producto])
+REFERENCES [dbo].[producto] ([id_producto])
+GO
+ALTER TABLE [dbo].[detalle] CHECK CONSTRAINT [id_producto]
+GO
+ALTER TABLE [dbo].[empleados]  WITH CHECK ADD  CONSTRAINT [FK_empleado_sucursal] FOREIGN KEY([id_sucursal])
+REFERENCES [dbo].[sucursal] ([id_sucursal])
+GO
+ALTER TABLE [dbo].[empleados] CHECK CONSTRAINT [FK_empleado_sucursal]
+GO
+ALTER TABLE [dbo].[factura]  WITH CHECK ADD  CONSTRAINT [id_cliente] FOREIGN KEY([id_cliente])
+REFERENCES [dbo].[cliente] ([id_cliente])
+GO
+ALTER TABLE [dbo].[factura] CHECK CONSTRAINT [id_cliente]
+GO
+ALTER TABLE [dbo].[factura]  WITH CHECK ADD  CONSTRAINT [id_detalle] FOREIGN KEY([id_detalle])
+REFERENCES [dbo].[detalle] ([id_detalle])
+GO
+ALTER TABLE [dbo].[factura] CHECK CONSTRAINT [id_detalle]
+GO
+ALTER TABLE [dbo].[producto]  WITH CHECK ADD  CONSTRAINT [id_categoria] FOREIGN KEY([id_categoria])
+REFERENCES [dbo].[categoria] ([id_categoria])
+GO
+ALTER TABLE [dbo].[producto] CHECK CONSTRAINT [id_categoria]
+GO
+USE [master]
+GO
+ALTER DATABASE [AkirasBoutiques] SET  READ_WRITE 
+GO
